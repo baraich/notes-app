b60d64533ee0c48f09dded4038783ffb3f7130a2
Baraich <gurvirsingh.baraich@gmail.com>
2025-08-02 15:15:47 +0530

    added a plugin to aid with some error on vercel from docs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+// eslint-disable-next-line
+// @ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig: NextConfig = {
   images: {},
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
