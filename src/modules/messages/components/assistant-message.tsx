1cab314627a4f3aa6ace64fe71b3a3b5dca71f81
Baraich <gurvirsingh.baraich@gmail.com>
2025-08-04 15:07:27 +0530

    basics down

--- src/modules/messages/components/assistant-message.tsx
+++ src/modules/messages/components/assistant-message.tsx
@@ -1,14 +1,24 @@
 "use client";
-import { useEffect, useState } from "react";
-import MapDisplay from "./map-display";
+import { JSX, useEffect, useState } from "react";
+import MapDisplayTool from "../../tools/components/map-display-tool";
 import { parseMarkdown } from "@/lib/markdown";
-import { ToolCall } from "@/modules/tools/interface";
+import { ToolCall, ValidTool } from "@/modules/tools/interface";
+import SearchTool from "@/modules/tools/components/search-tool";
 
 interface AssistantMessageProps {
   content: string;
   // eslint-disable-next-line
   toolCalls?: ToolCall<any>[];
 }
+
+const toolUi: Record<
+  ValidTool,
+  // eslint-disable-next-line
+  (tool: ToolCall<any>) => JSX.Element
+> = {
+  map: (tool: ToolCall<"map">) => <MapDisplayTool {...tool} />,
+  search: (tool: ToolCall<"search">) => <SearchTool {...tool} />,
+};
 
 export default function AssistantMessage({
   content,
@@ -45,15 +55,17 @@ export default function AssistantMessage({
               dangerouslySetInnerHTML={{ __html: cleanHtmlContent }}
             ></div>
           </div>
-          {toolCalls?.map((tool, idx) =>
-            tool.name == "map" ? (
-              <MapDisplay
-                key={idx}
-                input={tool.input}
-                output={tool.output}
-              />
-            ) : null
-          )}
+          {toolCalls?.map((tool, idx) => {
+            const Tool = toolUi[tool.name as ValidTool];
+            if (!Tool) {
+              return null;
+            }
+            return (
+              <div key={idx}>
+                <Tool {...tool} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
